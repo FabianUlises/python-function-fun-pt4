177375e2e805eadeb93d7645e8c0d226aa4ffa0c
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-06 12:44:13 -0700

    1

--- python-function-pt4.py
+++ python-function-pt4.py
@@ -1,4 +1,9 @@
 #1
-def max_num(x):
-    print(x)
-max_num(5)
+def max_num(a, b, c):
+    return max([a, b, c])
+print(max_num(1, 2, 3));
+
+#2
+def mult_list(*args):
+    print(args)
+mult_list(3,4,5)
